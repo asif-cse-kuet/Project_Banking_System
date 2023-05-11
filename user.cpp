f9904ae0d1c117f28663d4cc4e740b3a4b9181af
ASIF HASAN TONMOY <92232794+asif-cse-kuet@users.noreply.github.com>
2023-05-11 17:33:00 +0600

    Version 2.0

--- user.cpp
+++ user.cpp
@@ -22,14 +22,17 @@ public:
         current_balance = balance;
         initail_deposit = deposit;
     }
+
     int balance()
     {
         return current_balance;
     }
+
     void printUserInfo()
     {
         cout<<"\n"<<fname<<" info is given bellow :\n"<<"UserName: "<<username<<endl<<"Current Balance : "<<current_balance<<endl;
     }
+
     void ShowBalance(){
         cout<<"Your Personal Current Balance is : "<<current_balance<<endl;
     }
@@ -39,62 +42,31 @@ public:
         current_balance -= amount;
         ShowBalance();
     }
+
     void DepositBalance(int amount)
     {
         current_balance += amount;
         ShowBalance();
     }
-    void ShowUser(string usr_name, string usr_pass, UserAcc* person)
+
+    int ShowUser(string usr_name, string usr_pass)
     {
-        if((person->username == usr_name) && (person->password == usr_pass))
+        if((this->username == usr_name) && (this->password == usr_pass))
         {
             cout<<"You are successfully Logged In\nWelcome to the banking system\n"<<endl;
-            printUserInfo();
+//            printUserInfo();
+            return 1;
         }
         else
         {
             cout<<"\nYou have entered a wrong password or id. Create an account if you don't have any"<<endl;
+            return 0;
         }
     }
-};
 
-
-user::user()
-{
-    cout<<"Welcome to Banking System User Dashboard"<<endl;
-}
-
-int user::sum(int a, int b)
-{
-    return a+b;
-}
-
-int user::UserDashboard()
-{
-    while(1){
-        char options;
-        cout<<"\nPlease select the following options: \n\n1. Create User Account \n2. User LogIn\n3. Admin Login\n4. Exit"<<endl;
-        cin>>options;
-        UserAcc person1;
-        if(options == '1')
-        {
-            int login = 0;
-            string uname, fname, upass; int cash;
-            cout<<"\nEnter a username, password, deposit for your account"<<endl;
-            cout<<"User Name: ";
-            cin>>uname;
-            cout<<"Your First Name: ";
-            cin>>fname;
-            cout<<"User Pass: ";
-            cin>>upass;
-            cout<<"Deposit : ";
-            cin>>cash;
-            cout<<" Thank you for the details "<<endl;
-            login = 1;
-            person1.setUser(uname,fname,upass,cash,cash);
-            person1.printUserInfo();
-
-            while(login)
+    void loggedIn(int login)
+    {
+        while(login)
             {
                 char select;
                 cout<<"\nPlease select the following options: \n\n1. Balance Check \n2. CashWithDraw\n3. Deposit\n4. Exit"<<endl;
@@ -102,7 +74,7 @@ int user::UserDashboard()
                 if(select == '1')
                 {
                     char ch;
-                    person1.ShowBalance();
+                    this->ShowBalance();
                     cout<<"Want to exit now? (Y/N)"<<endl;
                     cin>>ch;
                     if(ch=='n'||ch=='N')
@@ -120,9 +92,9 @@ int user::UserDashboard()
                     //ChashWithDraw
                     cout<<"Enter Withdraw amount : ";
                     cin>>withdraw;
-                    if(person1.balance()>=withdraw)
+                    if(this->balance()>=withdraw)
                     {
-                        person1.WithdrawBalance(withdraw);
+                        this->WithdrawBalance(withdraw);
                     }
                     else
                     {
@@ -136,7 +108,7 @@ int user::UserDashboard()
                     cin>>moneydeposit;
                     if(moneydeposit>0)
                     {
-                        person1.DepositBalance(moneydeposit);
+                        this->DepositBalance(moneydeposit);
                     }
                     else
                     {
@@ -154,6 +126,49 @@ int user::UserDashboard()
                     break;
                 }
             }
+    }
+};
+
+
+
+
+
+user::user()
+{
+    cout<<"Welcome to Banking System User Dashboard"<<endl;
+}
+
+int user::sum(int a, int b)
+{
+    return a+b;
+}
+
+int user::UserDashboard()
+{
+    UserAcc person1;
+    while(1){
+        char options;
+        cout<<"\nPlease select the following options: \n\n1. Create User Account \n2. User LogIn\n3. Admin Login\n4. Exit"<<endl;
+        cin>>options;
+//        UserAcc person1;
+        if(options == '1')
+        {
+            int login = 0;
+            string uname, fname, upass; int cash;
+            cout<<"\nEnter a username, password, deposit for your account"<<endl;
+            cout<<"User Name: ";
+            cin>>uname;
+            cout<<"Your First Name: ";
+            cin>>fname;
+            cout<<"User Pass: ";
+            cin>>upass;
+            cout<<"Deposit : ";
+            cin>>cash;
+            cout<<" Thank you for the details "<<endl;
+            login = 1;
+            person1.setUser(uname,fname,upass,cash,cash);
+            person1.printUserInfo();
+            person1.loggedIn(login);
         }
         else if(options == '2')
         {
@@ -163,7 +178,15 @@ int user::UserDashboard()
             cin>>usr_name;
             cout<<"\nPassword : ";
             cin>>usr_pass;
-            person1.ShowUser(usr_name,usr_pass, &person1);
+            int check = person1.ShowUser(usr_name,usr_pass);
+            if(check)
+            {
+                person1.loggedIn(check);
+            }
+            else
+            {
+                continue;
+            }
         }
         else if(options == '3')
         {
